53b339a26ac460d97259579e1dea35e151064854
Paul Esch-Laurent <p@uly.io>
2023-07-15 01:07:37 -0500

    feat: show menu

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ async function getAPI() {
   const html = await res.text()
   try {
     const parser = new DOMParser()
-    const doc = parser.parseFromString(html, 'text/html');
-    const $scripts = Array.from(doc.querySelectorAll('script'))
+    const $dom = parser.parseFromString(html, 'text/html')
+    const $scripts = Array.from($dom.querySelectorAll('script'))
     const $script = $scripts.filter($script => $script.innerHTML.trim().startsWith('var _mp_require = {')).pop()
     const json = JSON.parse($script.innerHTML.trim().replace(/^var _mp_require =/, '').replace(/;$/, ''))
     return Promise.resolve(json['config']['js/page_roundup_location']['locations_url'])
@@ -35,9 +35,56 @@ async function getRestaurants(api) {
   return data
 }
 
+async function formatRestaurants(restaurants) {
+  return Promise.all(restaurants.map(async restaurant => {
+    const parser = new DOMParser()
+    const $html = parser.parseFromString(restaurant.html, 'text/html');
+    const [html, menu_] = formatRestaurantHTML($html)
+    const menu = await getMenu(menu_).catch(handleError)
+    return {
+      ...restaurant,
+      html,
+      menu
+    }
+  }))
+}
+
+function formatRestaurantHTML($html) {
+  $html.querySelectorAll('a[name]').forEach($el => $el.remove())
+  $html.querySelectorAll('.number').forEach($el => $el.remove())
+  $html.querySelectorAll('[itemprop=name]').forEach($el => $el.remove())
+  $html.querySelectorAll('img').forEach($img => {
+    $img.srcset = ''
+    $img.src = $img.src.replace(/\?.*$/, '')
+    $img.removeAttribute('width')
+    $img.removeAttribute('height')
+  })
+  $html.querySelectorAll('a[href^=http]').forEach($a => {
+    $a.target = '_blank'
+    $a.rel = 'noopener noreferrer'
+  })
+  $html.querySelectorAll('.info a').forEach($a => {
+    $a.classList.add('button')
+  })
+  const menu = Array.from($html.querySelectorAll('a')).find($a => $a.textContent.toLowerCase().trim() === 'menu').href
+  return [$html.querySelector('li').innerHTML, menu]
+}
+
+async function getMenu(api) {
+  const res = await fetch(proxy + api)
+  const html = await res.text()
+  const parser = new DOMParser()
+  const $html = parser.parseFromString(html, 'text/html')
+  return Promise.resolve(formatMenuHTML($html))
+}
+
+function formatMenuHTML($html) {
+  return $html.querySelector('.content').innerHTML
+}
+
 (async () => {
   const api = await getAPI().catch(handleError)
   const restaurants = await getRestaurants(api + '?page=1').catch(handleError)
   console.log(api, restaurants)
-  $app.innerHTML = Mustache.render($template.innerHTML, { restaurants })
+  $app.innerHTML = Mustache.render($template.innerHTML, { restaurants: await formatRestaurants(restaurants).catch(handleError) })
 })();
