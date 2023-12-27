e6150faf18ed042e89f8b2947ed06a6a11d7e463
Paul Esch-Laurent <p@uly.io>
2023-12-27 15:56:55 -0600

    fix: no leading slash

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // const url = 'https://corsproxy.io/?' + encodeURIComponent('https://mspmag.com/promotions/restaurantweek')
 const proxy = '/proxy/mspmag.com/'
-const url = '/promotions/restaurantweek/restaurant-week-2023/'
+const url = 'promotions/restaurantweek/restaurant-week-2023/'
 const $app = document.getElementById('app')
 const $template = document.getElementById('template')
 
